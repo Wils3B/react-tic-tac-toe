aefc595da2d1c37bd24e391c3feb956cc2a2aee9
Wilson Gouanet <wilstito@gmail.com>
2021-09-19 04:21:47 -0400

    add game logic in App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import GameBoard from './components/GameBoard/GameBoard';
+import { newGame } from './store/game/game-slice';
+import { currentPlayerSelector } from './store/game/game-selectors';
 
 function App() {
+  const currentPlayer = useSelector(currentPlayerSelector);
+  const dispatch = useDispatch();
+  const isTheGameOver = useSelector(state => state.game.isTheGameOver);
+  const winner = useSelector(state => state.game.winner);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="tic-tac-toe">
+      <header className="d-flex justify-content-between align-items-center mb-2">
+        <h2 className="mb-0">TicTacToe</h2>
+        <button
+          className="btn btn-primary btn-sm"
+          onClick={() => dispatch(newGame())}
         >
-          Learn React
-        </a>
+          New Gane
+        </button>
       </header>
+      <GameBoard />
+      <div className="mt-2">
+        {
+          !isTheGameOver ?
+          `${currentPlayer.name} has to play` :
+          ( winner ? `${winner.name} won!` : 'No winner' )
+        }
+      </div>
     </div>
   );
 }
